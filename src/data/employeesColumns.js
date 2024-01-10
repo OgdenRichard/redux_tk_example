3d30e100c641b8bea929b52cae51b0a3e2623c80
Richard Ogden <ogden.richard@gmail.com>
2024-01-10 16:25:53 +0100

    10012024 | cleanup + prepare branch react table

---
+++ src/data/employeesColumns.js
@@ -0,0 +1,38 @@
+export const COLUMNS = [
+  {
+    Header: 'First Name',
+    accessor: 'firstname',
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'lastname',
+  },
+  {
+    Header: 'Start Date',
+    accessor: 'startdate',
+  },
+  {
+    Header: 'Department',
+    accessor: 'department',
+  },
+  {
+    Header: 'Date of birth',
+    accessor: 'birthdate',
+  },
+  {
+    Header: 'Street',
+    accessor: 'street',
+  },
+  {
+    Header: 'City',
+    accessor: 'city',
+  },
+  {
+    Header: 'State',
+    accessor: 'state',
+  },
+  {
+    Header: 'Zip Code',
+    accessor: 'zipcode',
+  },
+];
