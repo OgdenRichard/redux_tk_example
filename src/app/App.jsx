c84780ce1789d65f7f689b554a60838228e52119
Richard Ogden <ogden.richard@gmail.com>
2024-01-30 15:35:37 +0100

    30012024 | switch dark/light mode

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useMemo } from 'react';
 import {
   createBrowserRouter,
   Route,
@@ -6,21 +7,19 @@ import {
   Navigate,
 } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import NightlightRoundOutlinedIcon from '@mui/icons-material/NightlightRoundOutlined';
+import LightModeIcon from '@mui/icons-material/LightMode';
 import CssBaseline from '@mui/material/CssBaseline';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Stack } from '@mui/material';
+import { grey } from '@mui/material/colors';
 import { Root } from '../layouts/Root';
 import { Home } from '../pages/Home';
 import { Employees } from '../pages/Employees';
 import { ErrorPage } from '../pages/ErrorPage';
 import '../style/App.css';
 import 'dayjs/locale/fr';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-});
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -34,12 +33,39 @@ const router = createBrowserRouter(
 );
 
 function App({ children }) {
+  const [mode, setMode] = useState('dark');
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fr">
         <RouterProvider router={router} />
         {children}
+
+        <Stack className="switchmode" direction="row" sx={{ mt: 1.5, ml: 1.5 }}>
+          {mode === 'dark' ? (
+            <LightModeIcon
+              fontSize="small"
+              style={{ color: grey[500] }}
+              onClick={() => setMode('light')}
+            />
+          ) : (
+            <NightlightRoundOutlinedIcon
+              fontSize="small"
+              style={{ color: grey[700] }}
+              onClick={() => setMode('dark')}
+            />
+          )}
+        </Stack>
       </LocalizationProvider>
     </ThemeProvider>
   );
