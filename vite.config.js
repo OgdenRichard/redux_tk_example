057224461d16be12277d85ee4b91ed9e58cfb536
Richard Ogden <ogden.richard@gmail.com>
2024-03-28 16:54:51 +0100

    28032024 | vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rog_14_0124/',
   plugins: [react()],
-})
+});
